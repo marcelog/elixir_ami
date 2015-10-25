5867b4ebe33b4e27a66d0293e2c0b4375ab19107
Marcelo Gornstein <marcelog@gmail.com>
2015-10-24 23:08:30 -0300

    adding some actions

--- lib/elixir_ami/action.ex
+++ lib/elixir_ami/action.ex
@@ -78,26 +78,76 @@ defmodule ElixirAmi.Action do
     }
   end
 
-  @spec logout() :: t
-  def logout() do
+  @doc """
+  See: https://wiki.asterisk.org/wiki/display/AST/ManagerAction_Logoff
+  """
+  @spec logoff() :: t
+  def logoff() do
     new "Logoff"
   end
 
+  @doc """
+  See: https://wiki.asterisk.org/wiki/display/AST/ManagerAction_Ping
+  """
   @spec ping() :: t
   def ping() do
     new "Ping"
   end
 
+  @doc """
+  See: https://wiki.asterisk.org/wiki/display/AST/ManagerAction_CoreSettings
+  """
   @spec core_settings() :: t
   def core_settings() do
     new "CoreSettings"
   end
 
+  @doc """
+  See: https://wiki.asterisk.org/wiki/display/AST/ManagerAction_CoreShowChannels
+  """
   @spec core_show_channels() :: t
   def core_show_channels() do
     new "CoreShowChannels"
   end
 
+  @doc """
+  See: https://wiki.asterisk.org/wiki/display/AST/ManagerAction_Getvar
+  """
+  @spec get_var(String.t, String.t) :: t
+  def get_var(channel, name) do
+    new "Getvar", %{
+      channel: channel,
+      variable: name
+    }
+  end
+
+  @doc """
+  See: https://wiki.asterisk.org/wiki/display/AST/ManagerAction_Reload
+  """
+  @spec reload(String.t) :: t
+  def reload(module) do
+    new "Reload", %{
+      channel: channel,
+      module: module
+    }
+  end
+
+  @doc """
+  See: https://wiki.asterisk.org/wiki/display/AST/ManagerAction_Setvar
+  """
+  @spec set_var(String.t, String.t, String.t) :: t
+  def set_var(channel, name, value) do
+    new "Setvar", %{
+      channel: channel,
+      variable: name,
+      value: to_string(value)
+    }
+  end
+
+  @doc """
+  Transforms an action structure into an iolist suitable to be sent through
+  a socket.
+  """
   @spec serialize(t) :: iolist
   def serialize(action) do
     [
@@ -108,6 +158,9 @@ defmodule ElixirAmi.Action do
     ]
   end
 
+  @doc """
+  Creates a new action structure from the given name, keys, and variables.
+  """
   @spec new(String.t, Map.t, Map.t) :: t
   def new(name, keys \\ %{}, variables \\ %{}) do
     %ElixirAmi.Action{
