790133cca9cfa9aad25752fce51f5cebf34b6ba7
Marcelo Gornstein <marcelog@gmail.com>
2015-11-18 13:45:48 -0300

    bumping up version

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ To use it in your Mix projects, first add it as a dependency:
 
 ```elixir
 def deps do
-  [{:elixir_ami, "~> 0.0.2"}]
+  [{:elixir_ami, "~> 0.0.3"}]
 end
 ```
 Then run mix deps.get to install it.
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElixirAmi.Mixfile do
     [
       app: :elixir_ami,
       name: "elixir_ami",
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.0",
       description: description,
       package: package,
