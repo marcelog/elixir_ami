6317df39e1ef7b64dd49784175fcf69794f2eb2b
Marcelo Gornstein <marcelog@gmail.com>
2016-08-24 11:18:11 -0300

    bumping up version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElixirAmi.Mixfile do
     [
       app: :elixir_ami,
       name: "elixir_ami",
-      version: "0.0.15",
+      version: "0.0.16",
       elixir: "~> 1.0",
       description: description,
       package: package,
